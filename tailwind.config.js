657daf2e61dd7340e73593e0fc9dd7bb67ea6e40
shahinchowdory02 <shaheenchowdory80250@gmail.com>
2024-06-17 14:57:41 +0600

    second commit

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      workbench: ["Workbench"],
+    },
+
+    fontFamily: {
+      dancing: ["Dancing Script"],
+    },
+
+    fontFamily: {
+      roboto: ["Roboto"],
+    },
+
+    fontFamily: {
+      sofia: ["Sofia"],
+    },
+
+    extend: {
+      rotate: {
+        170: "170deg",
+      },
+
+      keyframes: {
+        move: {
+          "50%": { transform: "translateY(-1rem)" },
+        },
+      },
+
+      animation: {
+        movingY: "move 2s linear infinite",
+      },
+    },
+  },
+
+  container: {
+    center: true,
+  },
+
+  plugins: [],
+};
